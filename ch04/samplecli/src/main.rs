f71ade696714724d1bcb5c34f9062ec3fb8a53ba
nekoTheShadow <h.nakamura0903@gmail.com>
2024-03-20 22:07:47 +0900

    4-2 計算ロジックを追加する

--- ch04/samplecli/src/main.rs
+++ ch04/samplecli/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::File,
+    io::{stdin, BufRead, BufReader},
+};
+
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -11,15 +16,72 @@ struct Opts {
     #[arg(short, long)]
     verbose: bool,
 
-    #[arg(name="FILE")]
+    #[arg(name = "FILE")]
     formula_file: Option<String>,
+}
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
 }
 
 fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified.")
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
     }
-    println!("Is verbosity specified?: {}", opts.verbose)
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer = calc.eval(&line);
+        println!("{}", answer);
+    }
 }
